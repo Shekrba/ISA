eb8c2155b3ed0288df7042dcb76da7e2617db8a7
Kickapoo <Kickapoo@169.254.126.5>
2018-12-27 07:50:18 +0100

    Konflikt resen

---
+++ projekat/src/main/java/isa/putujIgumane/model/korisnik/Rezervacije.java
@@ -0,0 +1,65 @@
+package isa.putujIgumane.model.korisnik;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import isa.putujIgumane.model.avioKompanija.Karta;
+import isa.putujIgumane.model.hotel.RezervacijaSobe;
+import isa.putujIgumane.model.rentACar.RezervacijaVozila;
+
+public class Rezervacije {
+	
+/*	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+//	@OneToOne(mappedBy = "rezervacija", optional = false)
+//	private Karta avionskaKarta = new Karta();
+	
+	private Set<RezervacijaSobe> rezervacijaSobe = new HashSet<RezervacijaSobe>();
+	
+	private Set<RezervacijaVozila> rezervacijaVozila = new HashSet<RezervacijaVozila>();
+	
+	private Set<Karta> avionskaKarta = new HashSet<Karta>();
+	
+	public Rezervacije(Long id, Set<Karta> avionskaKarta, Set<RezervacijaSobe> rezervacijaSobe,
+			Set<RezervacijaVozila> rezervacijaVozila) {
+		super();
+		this.id = id;
+		this.avionskaKarta = avionskaKarta;
+		this.rezervacijaSobe = rezervacijaSobe;
+		this.rezervacijaVozila = rezervacijaVozila;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Set<Karta> getAvionskaKarta() {
+		return avionskaKarta;
+	}
+	public void setAvionskaKarta(Set<Karta> avionskaKarta) {
+		this.avionskaKarta = avionskaKarta;
+	}
+	public Set<RezervacijaSobe> getRezervacijaSobe() {
+		return rezervacijaSobe;
+	}
+	public void setRezervacijaSobe(Set<RezervacijaSobe> rezervacijaSobe) {
+		this.rezervacijaSobe = rezervacijaSobe;
+	}
+	public Set<RezervacijaVozila> getRezervacijaVozila() {
+		return rezervacijaVozila;
+	}
+	public void setRezervacijaVozila(Set<RezervacijaVozila> rezervacijaVozila) {
+		this.rezervacijaVozila = rezervacijaVozila;
+	}*/
+	
+	
+}

dddd6c222de4efd0a85af8b2a5e109e31900a4fc
gori8 <goriantolovic@gmail.com>
2019-01-03 20:20:17 +0100

    Service anotacija

--- projekat/src/main/java/isa/putujIgumane/service/hotel/HotelService.java
+++ projekat/src/main/java/isa/putujIgumane/service/hotel/HotelService.java
@@ -2,12 +2,14 @@ package isa.putujIgumane.service.hotel;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import isa.putujIgumane.model.hotel.Hotel;
 
 
-
+@Service
 public interface HotelService {
 	
-	public List<Hotel> findAll();
+	public abstract List<Hotel> findAll();
 
 }
--- projekat/src/main/java/isa/putujIgumane/service/hotel/HotelServiceImpl.java
+++ projekat/src/main/java/isa/putujIgumane/service/hotel/HotelServiceImpl.java
@@ -3,15 +3,18 @@ package isa.putujIgumane.service.hotel;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import isa.putujIgumane.model.hotel.Hotel;
 import isa.putujIgumane.repository.hotel.HotelRepository;
 
+@Service
 public class HotelServiceImpl implements HotelService {
 	
 	@Autowired
 	private HotelRepository hotelRepository;
 	
+	@Override
 	public List<Hotel> findAll() {
 		return hotelRepository.findAll();
 	}

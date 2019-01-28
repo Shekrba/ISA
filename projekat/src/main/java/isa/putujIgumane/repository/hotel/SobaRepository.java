63061ce435d8437107b7233e81fe9d5182703c2d
gori8 <goriantolovic@gmail.com>
2019-01-28 08:59:14 +0100

    Datum skroz radi

--- projekat/src/main/java/isa/putujIgumane/repository/hotel/SobaRepository.java
+++ projekat/src/main/java/isa/putujIgumane/repository/hotel/SobaRepository.java
@@ -16,7 +16,7 @@ import isa.putujIgumane.model.hotel.Soba;
 @Repository
 public interface SobaRepository extends JpaRepository<Soba, Long> {
 	
-	@Query("SELECT DISTINCT s FROM Soba s JOIN FETCH s.statusSobe ss WHERE s.hotel.id=?1 AND ss.datum BETWEEN ?2 AND ?3 AND ss.zauzeto=false")
+	@Query("SELECT DISTINCT s FROM Soba s JOIN FETCH s.statusSobe ss WHERE s.hotel.id=?1 AND ss.datum>=?2 AND ss.datum<=?3 AND ss.zauzeto=false")
 	List<Soba> findFreeSobe(Long hotelId, LocalDate from, LocalDate to);
 	
 	HashSet<Soba> findByHotel(Hotel hotel);
